dc0dacecb462217e2fbf59d039bf71e2862943ad
Juhi Gupta <juhi.gupta@avantika.edu.in>
2024-04-06 15:10:38 +0530

    Update hollow right-triangle.py

--- hollow right-triangle.py
+++ hollow right-triangle.py
@@ -4,9 +4,7 @@ while(i<=n):
     j=1
     if i!=n:
         while(j<=i): 
-            if j==1 or j==i:
                 print("*", end=' ')
-            else:
                 print(' ', end=' ')
             j+=1
         print()
